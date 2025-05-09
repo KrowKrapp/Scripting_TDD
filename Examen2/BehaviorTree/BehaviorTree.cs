cc0c87ed74214cb617aee62ffdca203b2652f119
Kottar <valeria.quinteroc@upb.edu.co>
2025-05-08 22:50:21 -0500

    mejoras

--- Examen2/BehaviorTree/BehaviorTree.cs
+++ Examen2/BehaviorTree/BehaviorTree.cs
@@ -1,76 +1,6 @@
 ﻿using System;
 using System.Threading;
 
-abstract class Node
-{
-    public abstract bool Execute();
-}
-
-class Root : Node
-{
-    private Node child;
-    public Root(Node child)
-    {
-        if (child is Root) throw new InvalidOperationException("Root no puede tener otro Root como hijo.");
-        this.child = child;
-    }
-    public override bool Execute() => child?.Execute() ?? false;
-}
-
-abstract class Composite : Node
-{
-    protected Node[] children;
-    protected Composite(params Node[] children)
-    {
-        foreach (var child in children)
-        {
-            if (child is Root) throw new InvalidOperationException("Composite no puede tener Root como hijo.");
-        }
-        this.children = children;
-    }
-}
-
-class Sequence : Composite
-{
-    public Sequence(params Node[] children) : base(children) { }
-    public override bool Execute()
-    {
-        foreach (var child in children)
-            if (!child.Execute()) return false;
-        return true;
-    }
-}
-
-class Selector : Composite
-{
-    private Func<bool> condition;
-    public Selector(Func<bool> condition, params Node[] children) : base(children) { this.condition = condition; }
-    public override bool Execute()
-    {
-        if (condition != null && !condition()) return false;
-        foreach (var child in children)
-            if (child.Execute()) return true;
-        return false;
-    }
-}
-
-abstract class Task : Node
-{
-    protected Func<bool> action;
-    protected Task(Func<bool> action) { this.action = action; }
-}
-
-class MoveTask : Task
-{
-    public MoveTask(Func<bool> action) : base(action) { }
-    public override bool Execute() => action();
-}
-
-class WaitTask : Task
-{
-    public WaitTask(Func<bool> action) : base(action) { }
-    public override bool Execute() => action();
-}
 
 static int posicionPersonaje = 0;
 static int posicionObjetivo = 10;
@@ -101,3 +31,76 @@ static void Main()
     while (!arbol.Execute()) { }
     Console.WriteLine("Objetivo alcanzado!");
 }
+
+public abstract class Node
+{
+    public abstract bool Execute();
+}
+
+public class  Root : Node
+{
+    private Node child;
+    public Root(Node child)
+    {
+        if (child is Root) throw new InvalidOperationException("Root no puede tener otro Root como hijo.");
+        this.child = child;
+    }
+    public override bool Execute() => child?.Execute() ?? false;
+}
+
+public abstract class Composite : Node
+{
+    protected Node[] children;
+    protected Composite(params Node[] children)
+    {
+        foreach (var child in children)
+        {
+            if (child is Root) throw new InvalidOperationException("Composite no puede tener Root como hijo.");
+        }
+        this.children = children;
+    }
+}
+
+public class Sequence : Composite
+{
+    public Sequence(params Node[] children) : base(children) { }
+    public override bool Execute()
+    {
+        foreach (var child in children)
+            if (!child.Execute()) return false;
+        return true;
+    }
+}
+
+public class Selector : Composite
+{
+    private Func<bool> condition;
+    public Selector(Func<bool> condition, params Node[] children) : base(children) { this.condition = condition; }
+    public override bool Execute()
+    {
+        if (condition != null && !condition()) return false;
+        foreach (var child in children)
+            if (child.Execute()) return true;
+        return false;
+    }
+}
+
+public abstract class Task : Node
+{
+    protected Func<bool> action;
+    protected Task(Func<bool> action) { this.action = action; }
+}
+
+public class MoveTask : Task
+{
+    public MoveTask(Func<bool> action) : base(action) { }
+    public override bool Execute() => action();
+}
+
+class WaitTask : Task
+{
+    public WaitTask(Func<bool> action) : base(action) { }
+    public override bool Execute() => action();
+}
+
+
